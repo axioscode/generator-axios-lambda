9dd3299325c51ad7a87e1bdb397e93bde478cbe4
Nicholas Duffy <duffn@outlook.com>
2019-08-03 15:19:13 -0600

    Comment on exported items

--- generators/go/templates/functions/lambda_tmpl/main.go
+++ generators/go/templates/functions/lambda_tmpl/main.go
@@ -6,16 +6,19 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// Request is a Lambda request.
 type Request struct {
 	ID    float64 `json:"id"`
 	Value string  `json:"value"`
 }
 
+// Response is a Lambda response.
 type Response struct {
 	Message string `json:"message"`
 	Ok      bool   `json:"ok"`
 }
 
+// Handler processes the Lambda request.
 func Handler(request Request) (Response, error) {
 	return Response{
 		Message: fmt.Sprintf("Processed request ID %f", request.ID),
